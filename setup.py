8bf90ec3de9e4a8425ed1500898581cc64024698
Codingplace42 <jangascodingplace@gmail.com>
2021-05-02 16:56:49 +0200

    update version

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 import os
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
-
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-simple-chatbot",
-    version="0.0.4",
+    version="0.0.5",
     author="Janga",
     license='MIT License',
     author_email="jangascodingplace@gmail.com",
     description="A very basic Django Chatbot ft. NLTK and DRF",
-    long_description=README,
-    long_description_content_type="text/markdown",
     url="https://github.com/Codingplace42/django-simple-chatbot",
     packages=find_packages(),
     classifiers=[
--- simple_chatbot/__init__.py
+++ simple_chatbot/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 0, 4)
+VERSION = (0, 0, 5)
 __version__ = ".".join(str(i) for i in VERSION)
 
 default_app_config = "simple_chatbot.apps.SimpleChatbotConfig"
